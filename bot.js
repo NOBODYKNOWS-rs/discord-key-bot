ad618f03b6b1f8f8416d883bae263c017f341949
NOBODYKNOWS-rs <realsigmapsycho@gmail.com>
2026-03-05 20:46:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,12 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => res.send("Bot is running!"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
 const { Client, GatewayIntentBits } = require("discord.js")
 const fs = require("fs")
 
